a16ad355419dd30848c1779ecc23d7e2534b1e4c
Regina Steffen <reginasteffen@live.com>
2024-07-22 22:11:58 -0500

    created tables for galaxies and moons, planets now references those tables

--- outer_space.sql
+++ outer_space.sql
@@ -7,22 +7,61 @@ CREATE DATABASE outer_space;
 
 \c outer_space
 
+CREATE TABLE galaxies
+(
+  id SERIAL PRIMARY KEY, 
+  name TEXT NOT NULL
+);
+
+
 CREATE TABLE planets
 (
   id SERIAL PRIMARY KEY,
   name TEXT NOT NULL,
   orbital_period_in_years FLOAT NOT NULL,
   orbits_around TEXT NOT NULL,
-  galaxy TEXT NOT NULL,
-  moons TEXT[]
+  galaxy_id INTEGER REFERENCES galaxies(id)
 );
 
+CREATE TABLE moons
+(
+  id SERIAL PRIMARY KEY, 
+  name TEXT NOT NULL,
+  planet_id INTEGER REFERENCES planets(id)
+);
+
+
+ INSERT INTO galaxies (name)
+  VALUES
+  ('Milky Way');
+
 INSERT INTO planets
-  (name, orbital_period_in_years, orbits_around, galaxy, moons)
+  (name, orbital_period_in_years, orbits_around, galaxy_id)
 VALUES
-  ('Earth', 1.00, 'The Sun', 'Milky Way', '{"The Moon"}'),
-  ('Mars', 1.88, 'The Sun', 'Milky Way', '{"Phobos", "Deimos"}'),
-  ('Venus', 0.62, 'The Sun', 'Milky Way', '{}'),
-  ('Neptune', 164.8, 'The Sun', 'Milky Way', '{"Naiad", "Thalassa", "Despina", "Galatea", "Larissa", "S/2004 N 1", "Proteus", "Triton", "Nereid", "Halimede", "Sao", "Laomedeia", "Psamathe", "Neso"}'),
-  ('Proxima Centauri b', 0.03, 'Proxima Centauri', 'Milky Way', '{}'),
-  ('Gliese 876 b', 0.23, 'Gliese 876', 'Milky Way', '{}');
+  ('Earth', 1.00, 'The Sun', 
+  (SELECT id FROM galaxies WHERE name = 'Milky Way')),
+  ('Mars', 1.88, 'The Sun', 1),
+  ('Venus', 0.62, 'The Sun', 1),
+  ('Neptune', 164.8, 'The Sun', 1),
+  ('Proxima Centauri b', 0.03, 'Proxima Centauri', 1),
+  ('Gliese 876 b', 0.23, 'Gliese 876', 1);
+
+  INSERT INTO moons (name, planet_id)
+  VALUES
+  ('The Moon', 1), 
+  ('Phobos', 2), 
+  ('Deimos', 2), 
+  ('Naiad', 3),
+  ('Thalassa', 3),
+  ('Despina', 3), 
+  ('Galatea', 3),
+  ('Larissa', 3),
+  ('S/2004 N 1', 3), 
+  ('Proteus', 3), 
+  ('Triton', 3),
+  ('Nereid', 3), 
+  ('Halimede', 3),
+  ('Sao', 3),
+  ('Laomedeia', 3),
+  ('Psmathe', 3),
+  ('Neso', 3);
